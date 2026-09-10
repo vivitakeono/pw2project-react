1c8b6f90088eb453e3a4ebae9f11883a847f2c5a
wagnermarques <wagnerdocri@gmail.com>
2026-09-09 22:27:43 -0300

    09-09-26

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App (){
+   return (
+      <div className="bg-light min-vh-100 py-4">
+        <div> className ="container"
+            <header className="row mb-4"></header>
+              <div className="col text-center>"
+                <h1 className="display-5 fw-bold text-primary">
+                      Projeto React + Bootstrap Grid
+                </h1>
+                  <p className="lead text-secondary">
+                    Exemplo de layout responsivo utilizando o sistema de colunas do Bootstrap
+                  </p>
+        </div>
+      </div>
+    );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.createRoot(document.getElementyById('root')).render(
+    <React.strictMode>
+        <App/>
+    </React.strictMode>
+);
